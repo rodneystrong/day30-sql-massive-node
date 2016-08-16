b3ba6c294c917837cfcf7eb98313de21936b5c0a
aleeex <akang2@gmail.com>
2016-08-16 15:22:10 -0600

    created SQL file and connected to DB

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 var express = require('express');
+var massive = require('massive');
+
+//now we got psql runnin in terminal.
+// \l will pull up list of current dbs.
+// \c to connect
+//create database yourdbname; is the command you run to create a new db
+
+//after you created db and made your temp.sql file, run this: psql -f temp.sql;
 
 var app = express();
 
---
+++ temp.sql
@@ -0,0 +1,11 @@
+\c sql_massive_node;
+-- create table users(
+--   id serial primary key,
+--   name varchar(60),
+--   age integer,
+--   job varchar(20),
+--   height integer
+-- );
+
+insert into users(name, age, job, height)
+  value("alex", 30, "developer", 6);
