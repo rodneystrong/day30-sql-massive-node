3d2774e7da5723f7e47539cad3b9e8b4509547fd
aleeex <akang2@gmail.com>
2016-08-25 21:07:33 -0600

    fixed whatevs scope to include .whatevs in the controller, tried to display data differently by calling data.name on the view but didn't work

--- public/js/MainCtrl.js
+++ public/js/MainCtrl.js
@@ -2,7 +2,7 @@ angular
   .module('mikesFront')
   .controller('MainCtrl', function($scope, mainService) {
     $scope.whatevs = 'whatevs';
-    console.log(whatevs);
+    console.log($scope.whatevs);
 
     $scope.getData = function() {
       mainService.getData().then(function(response) {
