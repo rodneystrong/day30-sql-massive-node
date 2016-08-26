3b4fc029e0fdeec2d8bd390312d638294cbc3833
aleeex <akang2@gmail.com>
2016-08-25 20:39:01 -0600

    set up rest of angular front end and connected app controller service to index.html

---
+++ public/js/MainCtrl.js
@@ -0,0 +1,5 @@
+angular
+  .module('mikesFront')
+  .controller('MainCtrl', function($scope) {
+    $scope.whatevs = 'whatevs';
+  })
---
+++ public/js/mainService.js
@@ -0,0 +1,5 @@
+angular
+  .module('mikesFront')
+  .service('mainService', function() {
+    
+  })
